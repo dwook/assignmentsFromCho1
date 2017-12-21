e6215be1f1906ebd893bb64a2e39e65e121d3d97
dwook <igerapex@gmail.com>
2017-12-22 00:15:56 +0900

    second

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 
-$("aside li").on("mouseenter", function(){
-    
+var $header = $("header")
+var $logo =$(".logo")
+var $gnb = $("header nav li")
+var $menu = $("header nav a")
+
+$gnb.on("mouseenter", function(){
    
+   $header.css({
+       "background" : "#fff", 
+       "border-bottom":"1px solid #e5e5e5"
+    })
+   $menu.css("color", "#1b2252")
+   $logo.css("color", "#1b2252")
+   $(this).css("background", "#f7f9fd")
+   $(this).find("a").css("color", "#59c6fd")
+
 })
 
-$("aside li").on("mouseleave", function(){
+$gnb.on("mouseleave", function(){
     
-   
+    $header.css({
+        "background" : "transparent", 
+        "border-bottom" : "none" 
+    })
+    $menu.css({"color" : "#fff"})
+    $logo.css({"color" : "#fff"})
+    $gnb.css({
+        "background":"transparent",
+        "color":"#fff"
+    })
 })
