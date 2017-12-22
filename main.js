73063f82c01569d82bcc45dad32e09927b322c76
dwook <igerapex@gmail.com>
2017-12-22 21:49:47 +0900

    22 night

--- main.js
+++ main.js
@@ -1,48 +1,26 @@
 
-var $header = $("header")
-var $logo =$(".logo")
-var $gnb = $("header nav li")
-var $menu = $("header nav a")
+var $header = $(".header");
+var $gnb = $(".header nav > ul > li");
 var $devs = $(".dev > li");
 var $currentDev = $devs.eq(0);
 
-
 $gnb.on("mouseenter", function(){
-   
-   $header.css({
-       "background" : "#fff", 
-       "border-bottom":"1px solid #e5e5e5"
-    })
-   $menu.css("color", "#1b2252")
-   $logo.css("color", "#1b2252")
-   $(this).css("background", "#f7f9fd")
-   $(this).find("a").css("color", "#59c6fd")
-
-   if ( $(this).hasClass("devNav") ) {
-        $(".dev").slideDown();
-   }
-
-})
-
-$gnb.on("mouseleave", function(){
-    
-    $header.css({
-        "background" : "transparent", 
-        "border-bottom" : "none" 
-    })
-    $menu.css({"color" : "#fff"})
-    $logo.css({"color" : "#fff"})
-    $gnb.css({
-        "background":"transparent",
-        "color":"#fff"
-    })
-
+    $header.addClass("on")  
+    if ( $(this).hasClass("devNav") ) {   
+        $(this).addClass("on").find(".dev").stop().slideDown()
+    } else {
+        $(this).addClass("on")
+    }
+}).on("mouseleave", function(){
+    $header.removeClass("on")
     if ( $(this).hasClass("devNav") ) {
-        $(".dev").slideUp();
-   }
+        $(this).find(".dev").stop().hide().parent(".devNav").removeClass("on")
+    } else {
+        $(this).removeClass("on")
+    }
 })
 
-
+    
 $currentDev.addClass("on");
 $devs.on("click", function(){
     $currentDev.removeClass("on");
@@ -50,10 +28,4 @@ $devs.on("click", function(){
     $currentDev.addClass("on");     
     }
 );
-
-/*
-$(".devNav, .dev").hover(function(){
-    $(".dev").stop().slideToggle();
-})
-*/
 
