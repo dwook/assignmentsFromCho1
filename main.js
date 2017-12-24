e126baeeff079f85db00859df1bb84cdc4240588
dwook <igerapex@gmail.com>
2017-12-24 17:40:11 +0900

    1224 noon

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
 var $header = $(".header");
 var $gnb = $(".header nav > ul > li");
-var $devs = $(".dev > li");
-var $currentDev = $devs.eq(0);
 
 $gnb.on("mouseenter", function(){
     $header.addClass("on")  
@@ -20,6 +18,9 @@ $gnb.on("mouseenter", function(){
     }
 })
 
+
+var $devs = $(".dev > li");
+var $currentDev = $devs.eq(0);
     
 $currentDev.addClass("on");
 $devs.on("click", function(){
@@ -28,4 +29,40 @@ $devs.on("click", function(){
     $currentDev.addClass("on");     
     }
 );
+
+var $tab1_list = $(".cpnt1 .tab li")
+var $currentTab1 = $tab1_list.eq(0)
+var $reversePoint = $tab1_list.eq(2)
+
+$currentTab1.addClass("on").find(".inner").addClass("on");
+$tab1_list.on("click focusin", function(){
+
+  $currentTab1.removeClass("on").find(".inner").stop().animate({"left":"-100%"},"linear").css("left","100%");
+  $currentTab1 = $(this);
+  $currentTab1.addClass("on").find(".inner").addClass("on").css("left","100%").stop().animate({"left":"0px"},"linear");
+  }
+)
+
+
+$currentTab1.addClass("on").find(".inner").addClass("on");
+$tab1_list.on("click focusin", function(){
+    
+  $currentTab1.removeClass("on").find(".inner").stop().animate({"left":"-100%"},"linear").css("left","100%");
+  $currentTab1 = $(this);
+  $currentTab1.addClass("on").find(".inner").addClass("on").css("left","100%").stop().animate({"left":"0px"},"linear");
+  }
+)
+
+$(document).on('keydown', $reversePoint, function(e) {
+    if (e.which == 9) {
+        e.preventDefault();
+
+        $tab1_list.on("focusin", function(){
+            $currentTab1.removeClass("on").find(".inner").stop().animate({"left":"-100%"},"linear").css("left","100%");
+            $currentTab1 = $(this);
+            $currentTab1.addClass("on").find(".inner").addClass("on").css("left","100%").stop().animate({"left":"0px"},"linear");
+            }
+        
+    }
+});
 
